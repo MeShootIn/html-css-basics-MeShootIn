3a9daf1d592525b28cfa3487a48195f3112c0e7c
MeShootIn <dmitriimishutin@gmail.com>
2022-08-05 00:09:58 +0500

    Refactor code and translate comments

--- script/form.js
+++ script/form.js
@@ -1,9 +1,8 @@
 function onSubmit() {
-    const form = document.getElementById('feedback-form');
-    const inputs = form.elements;
+  const form = document.getElementById('feedback-form');
+  const inputs = form.elements;
 
-    inputs['subject'].value = decodeURI(inputs['subject'].value);
-    inputs['message'].value = decodeURI(inputs['message'].value);
-
-    form.submit();
+  inputs['subject'].value = decodeURI(inputs['subject'].value);
+  inputs['message'].value = decodeURI(inputs['message'].value);
+  form.submit();
 }
